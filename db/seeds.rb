f9fc2c4a78c6cb23d4e6ec38905cec21223bb9d9
natron19 <nathan19@hotmail.com>
2015-05-25 16:22:02 -0700

    added seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Author.create!(first_name: 'Agatha', last_name: 'Johnson')
+Author.create!(first_name: 'Bob', last_name: 'Christie')
+Author.create!(first_name: 'Fed', last_name: 'Fernandez')
+Author.create!(first_name: 'Jan', last_name: 'Chase')
+Author.create!(first_name: 'Laura', last_name: 'Tomlinson')
+Author.create!(first_name: 'Iggy', last_name: 'Croll')
+
+
+Publisher.create!([{name: 'Wiley'}, {name: 'Addison Wesley'}, 
+                  {name: 'Peachpit Press'}, {name: 'Manning'}, 
+                  {name: 'No Starch Press'}, {name: 'Apress'}
+                  ])
+
+Book.create!(title: 'Gnuplot in Action', isbn: '9999999999999', page_count: 518, price: 49.99, 
+                  description: 'A very cool software book', published_at: '2012-01-01', 
+                  publisher_id: 3, book_cover: File.open(File.join(Rails.root, 'app/assets/images/gnuplot.jpg')))
